a06663922b4c116b390cc8b27c265487e1abae82
josecarlosLH <josecarlos.lopez@internaliagroup.com>
2022-05-25 18:51:16 +0200

    Added MapView to the main ViewController

--- OpenWeatherIOS/ViewController.swift
+++ OpenWeatherIOS/ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 
